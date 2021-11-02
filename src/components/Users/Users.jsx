c0a617dbd3f07215b87207e89b09169d7553febb
79602 <kluchi_peter@mail.ru>
2021-11-02 17:07:41 +0300

    58 - React JS - mapDispatchToProps лайф-хак Signed-off-by: 79602 <kluchi_peter@mail.ru>
    Signed-off-by: 79602 <kluchi_peter@mail.ru>

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,41 +1,22 @@
 import React from "react";
 import s from './Users.module.css';
-import axios from "axios";
 import userPhoto from '../../assets/images/user.png';
 
-class Users extends React.Component {
-
-    componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
-            this.props.setUsers(response.data.items);
-            this.props.setTotalUsersCount(response.data.totalCount);
-        });
+const Users = (props) => {
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+    let pages = [];
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i);
     }
-
-    onPageChanget = (pageNumber) => {
-        this.props.setCurrentPage(pageNumber);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
-            this.props.setUsers(response.data.items)
-        });
-    }
-
-    render() {
-
-        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
-        let pages = [];
-        for (let i=1; i <= pagesCount; i++) {
-            pages.push(i);
-        }
-
     return <div>
         <div className={s.pages}>
             {pages.map(p => {
-                return <span className={this.props.currentPage === p && s.selectedPage }
-                             onClick={ (e) => { this.onPageChanget(p); }}>{p}</span>
+                return <span className={props.currentPage === p && s.selectedPage }
+                             onClick={ (e) => { props.onPageChanget(p); }}>{p}</span>
             })}
         </div>
         {
-            this.props.users.map(u => <div key={u.id}>
+            props.users.map(u => <div key={u.id}>
                 <span>
                     <div >
                         <img  src={u.photos.small != null ? u.photos.small : userPhoto} className={s.userPhoto} />
@@ -44,8 +25,8 @@ class Users extends React.Component {
                 <span>
                     <div>
                         {u.followed
-                            ? <button onClick={ () => { this.props.unfollow(u.id) } }>Unfollow</button>
-                            : <button onClick={ () => { this.props.follow(u.id) } }>Follow</button>
+                            ? <button onClick={ () => { props.unfollow(u.id) } }>Unfollow</button>
+                            : <button onClick={ () => { props.follow(u.id) } }>Follow</button>
                         }
                     </div>
                 </span>
@@ -64,7 +45,7 @@ class Users extends React.Component {
         }
     </div>
 }
-}
+
 
 
 
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,18 +1,63 @@
 import React from "react";
 import {connect} from "react-redux";
 import Users from "./Users";
-import {followAC, setCurrentPageAC, setUsersAC, unfollowAC, setUsersTotalCountAC} from "../../Redux/Users-reduser";
+import {
+    follow,
+    setCurrentPage,
+    setUsers,
+    unfollow,
+    setTotalUsersCount,
+    toggleIsFetching
+} from "../../Redux/Users-reduser";
+import axios from "axios";
+import Preloader from "../common/Preloader/Preloader";
+
+class UsersConteiner extends React.Component {
+
+    componentDidMount() {
+        this.props.toggleIsFetching(true);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+            this.props.toggleIsFetching(false);
+            this.props.setUsers(response.data.items);
+            this.props.setTotalUsersCount(response.data.totalCount);
+        });
+    }
+
+    onPageChanget = (pageNumber) => {
+        this.props.toggleIsFetching(true);
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
+            this.props.toggleIsFetching(false);
+            this.props.setUsers(response.data.items)
+        });
+    }
+
+    render() {
+        return <>
+            {this.props.isFetching ? <Preloader /> : null}
+            <Users currentPage={this.props.currentPage}
+                      onPageChanget={this.onPageChanget}
+                      users={this.props.users}
+                      unfollow={this.props.unfollow}
+                      follow={this.props.follow}
+                      totalUsersCount={this.props.totalUsersCount}
+                      pageSize={this.props.pageSize}
+                      />
+        </>
+    }
+}
 
 let mapStateToProps = (state) => {
     return {
     users: state.usersPage.users,
     pageSize: state.usersPage.pageSize,
     totalUsersCount: state.usersPage.totalUsersCount,
-        currentPage: state.usersPage.currentPage
+    currentPage: state.usersPage.currentPage,
+    isFetching: state.usersPage.isFetching
     }
 };
 
-let mapDispatchToProps = (dispatch) => {
+/*let mapDispatchToProps = (dispatch) => {
     return {
         follow: (usersId) => {
             dispatch(followAC(usersId));
@@ -28,8 +73,12 @@ let mapDispatchToProps = (dispatch) => {
         },
         setTotalUsersCount: (totalCount) => {
             dispatch(setUsersTotalCountAC(totalCount));
+        },
+        toggleIsFetching: (isFetching) => {
+            dispatch(setToggleIsFetchingAC(isFetching));
         }
     }
-}
+}*/
 
-export default connect(mapStateToProps, mapDispatchToProps)(Users);
+export default connect(mapStateToProps, {follow, unfollow, setUsers,
+    setCurrentPage, setTotalUsersCount, toggleIsFetching})(UsersConteiner);
